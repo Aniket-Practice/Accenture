e51200bbdb26e3fb367da60ea8a50f72dc656472
Aniket Practice <waghmareaniket150@gmail.com>
2024-03-08 22:12:55 +0530

    Latest Update

---
+++ Automation_Tester.java
@@ -0,0 +1,8 @@
+package Accenture;
+
+public class Automation_Tester {
+    public static void main(String[] args) {
+        System.out.println("Automation Engineer at ACCENTURE");
+        System.out.println("Remember: I will be RICH soon");
+    }
+}
